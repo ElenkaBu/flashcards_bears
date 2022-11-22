19b6f4c48ded4976689fcfca82deff4f780e3fd9
Naida <satana_dance@mail.ru>
2022-11-22 18:58:36 +0500

    CSS

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,6 +1,6 @@
 const React = require('react');
 
-function Layout({ children, classTag }) {
+function Layout({ children }) {
   return (
 
     <html lang="en">
@@ -9,23 +9,20 @@ function Layout({ children, classTag }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous" />
-        {/* <link rel="stylesheet" href="/public/styles/main.css" /> */}
+        <link rel="stylesheet" href="/styles/main.css" />
         <title>Flash Cards</title>
       </head>
       <header>
         <ul className="nav justify-content-center">
           <li className="nav-item">
-            <a className="nav-link active" href="/home">Home</a>
+            <a className="nav-link active" href="/home">| Home |</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/games">Game</a>
+            <a className="nav-link" href="/games">| Game |</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/score">Score</a>
+            <a className="nav-link" href="/score">| Score |</a>
           </li>
-          {/* <li className="nav-item">
-            <a className="nav-link disabled">Отключенная</a>
-          </li> */}
         </ul>
 
       </header>
--- src/views/Welcome.jsx
+++ src/views/Welcome.jsx
@@ -5,11 +5,11 @@ function Welcome() {
   return (
     <Layout classTag="welcome">
       <main className="welcome">
-        <br/>
-          <h1>Добро пожаловать в игру!</h1>
-          <br/>
+        <br />
+        <h1>Добро пожаловать в игру!</h1>
+        <br />
+        <img src="../../../img/Caaat.png" width="400px" />
         <a href="/home" className="btn btn-primary">Регистрация</a>
-
       </main>
     </Layout>
   );
